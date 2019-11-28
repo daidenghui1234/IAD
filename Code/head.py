e3b0f444bd2c51a86013f1c87b67a61663b2fa85
daidenghui1234 <3276336032@qq.com>
2019-11-28 21:03:48 +0800

    最终头文件

--- Code/head.py
+++ Code/head.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python     #在文件头部 ( 第一行 ) 加上   设置 Python 解释器  
 
 # -*- coding: utf-8 -*-  #在文件头部 ( 第二行 ) 加上   在编辑器中设置以 UTF-8 默认编码保存文件  
- 
+
+##############################展示广告图片#########################################
 
 import cv2 as cv
 import numpy as np
@@ -9,20 +10,20 @@ import glob
 import random
 
 ##############################展示广告图片#########################################
-#cv.namedWindow("advertising",cv.WINDOW_AUTOSIZE)  #定义窗口 /以一个参数是窗口名称 //第二个参数是窗口形式
-#cv.resizeWindow("advertising",1000,1500)
+cv.namedWindow("advertising",cv.WINDOW_AUTOSIZE)  #定义窗口 /以一个参数是窗口名称 //第二个参数是窗口形式
+cv.resizeWindow("advertising",1000,1500)
 def read_file(path_file_number):
     if path_file_number == []:
-      print("未找到广告文件")
+      print("未找到图片文件")
     else:
-        num =random.randint(0,len(path_file_number))
+        num =random.randint(0,len(path_file_number)-1)
         src = cv.imread(path_file_number[num])
         cv.imshow("advertising",src)
-        cv.waitKey(50)
+        cv.waitKey(3000)
 ###################################################################################
 def video_input(path_file_video_number):  #定义视频函数
     if path_file_video_number == []:
-      print("未找到广告文件")
+      print("未找到视频文件")
     else:
         num =random.randint(0,len(path_file_video_number)-1)
         capture = cv.VideoCapture(path_file_video_number[num]) #打开摄像头 /参数是数字表示摄像头 也可以是视频路径
@@ -30,170 +31,107 @@ def video_input(path_file_video_number):  #定义视频函数
             ret , frame = capture.read()  #获取视频信息 \第一个返回值表示视频状态 \第二个返回值表示每一帧画面
             if ret == False:
                 break
-#             frame = np.flip(frame,1)  #对画面进行颠倒 /画面信息 //1：左右，-1上下
             cv.imshow("advertising",frame)  #展示图片信息
-            cv.waitKey(10)
+            cv.waitKey(50)
 ##################################对应女性的广告投放################################
-def Male_0_2():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/0_2/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/0_2/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
+def aaa(path_file_video_number,path_file_number,a):
+    if a%10:
+        video_input(path_file_video_number)
+    if a//10:
+        read_file(path_file_number)
+        
 
-def Male_4_6():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/4_6/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/4_6/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_8_12():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/8_12/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/8_12/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_15_20():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/15_20/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/15_20/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_25_32():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/25_32/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/25_32/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_38_43():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/38_43/*.jpg") #获取当前文件夹下文
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/38_43/*.mp4") #获取当前文件夹下文
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_48_53():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/48_53/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/48_53/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Male_60_100():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Male/60_100/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Male/60_100/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-###################################################################################
-
-##################################对应男性的广告投放################################
-def Female_0_2():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/0_2/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/0_2/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_4_6():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/4_6/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/4_6/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_8_12():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/8_12/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/8_12/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_15_20():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/15_20/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/15_20/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_25_32():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/25_32/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/25_32/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_38_43():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/38_43/*.jpg") #获取当前文件夹下文
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/38_43/*.mp4") #获取当前文件夹下文
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_48_53():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/48_53/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/48_53/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-def Female_60_100():
-    path_file_number=glob.glob("/home/pi/IAD/AD/Female/60_100/*.jpg") #获取当前文件夹下文件
-    path_file_video_number=glob.glob("/home/pi/IAD/AD/Female/60_100/*.mp4") #获取当前文件夹下文件
-    video_input(path_file_video_number)
-    read_file(path_file_number)
-
-#######################################################################################
-
-
-###################################根据性别和年龄播放相应广告###########################
-def advertisements(Gender,Age):
+def advertisements(Gender,Age,aa):
 ####################################男性###############################################
     if Gender == "Male":
         if Age =="(0-2)":
-            Male_0_2()
+            number=glob.glob("/home/pi/IAD/AD/Male/0_2/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/0_2/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(4-6)":
-            Male_4_6()
+            number=glob.glob("/home/pi/IAD/AD/Male/4_6/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/4_6/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(8-12)":
-            Male_8_12()
+            number=glob.glob("/home/pi/IAD/AD/Male/8_12/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/8_12/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(15-20)":
-            Male_15_20()
+            number=glob.glob("/home/pi/IAD/AD/Male/15_20/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/15_20/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(25-32)":
-            Male_25_32()
+            number=glob.glob("/home/pi/IAD/AD/Male/25_32/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/25_32/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(38-43)":
-            Male_38_43()
+            number=glob.glob("/home/pi/IAD/AD/Male/38_43/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/38_43/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(48-53)":
-            Male_48_53()
+            number=glob.glob("/home/pi/IAD/AD/Male/48_53/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/48_53/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(60-100)":
-            Male_60_100()
+            number=glob.glob("/home/pi/IAD/AD/Male/60_100/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Male/60_100/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
         
 #####################################女性################################################
     elif Gender == "Female":
         if Age =="(0-2)":
-            Female_0_2()
+            number=glob.glob("/home/pi/IAD/AD/Female/0_2/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/0_2/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(4-6)":
-            Female_4_6()
+            number=glob.glob("/home/pi/IAD/AD/Female/4_6/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/4_6/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(8-12)":
-            Female_8_12()
+            number=glob.glob("/home/pi/IAD/AD/Female/8_12/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/8_12/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(15-20)":
-            Female_15_20()
+            number=glob.glob("/home/pi/IAD/AD/Female/15_20/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/15_20/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(25-32)":
-            Female_25_32()
+            number=glob.glob("/home/pi/IAD/AD/Female/25_32/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/25_32/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(38-43)":
-            Female_38_43()
+            number=glob.glob("/home/pi/IAD/AD/Female/38_43/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/38_43/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(48-53)":
-            Female_48_53()
+            number=glob.glob("/home/pi/IAD/AD/Female/48_53/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/48_53/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 
         if Age =="(60-100)":
-            Female_60_100()
+            number=glob.glob("/home/pi/IAD/AD/Female/60_100/*.jpg") #获取当前文件夹下文件
+            video=glob.glob("/home/pi/IAD/AD/Female/60_100/*.mp4") #获取当前文件夹下文件
+            aaa(video,number,aa)
 #######################################################################################
-
+#advertisements("Female","(48-53)",10)
+#######################################################################################
 
 # import sys   
 
 # reload(sys)   
 
 # sys.setdefaultencoding('utf-8')
+
